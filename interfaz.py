2f80a146c3908adba2647e4c8f2d9930e9d4c995
Esteban Sibaja <sibajajaja@gmail.com>
2021-01-18 14:44:53 -0600

    Create interfaz.py

---
+++ interfaz.py
@@ -0,0 +1,14 @@
+#elaborado por Esteban Sibaja y Diego Vega
+#fecha de creacion: x/x/2020 8:34pm
+#ultima modificacion: x/x/2020 11:05pm
+#version: 3.8.6
+#importacion  de librerias
+import tkinter
+from tkinter import *
+import pip
+from pip import *
+#VENTANA DEL PROGRAMA
+ventana = Tk() # crea una ventana
+ventana.title("ventana principal")#titulo de la ventana
+ventana.geometry("900x800")#tamaño de la ventana
+ventana.resizable(True, True)#opcionde modifacar el tamaño
